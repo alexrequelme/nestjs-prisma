7a04debf1a102bb7cd80266e7f0fdd8da65a8a2b
AlexRequelme <alexdeveloper09@gmail.com>
2023-10-03 11:52:59 -0500

    Hide exception filter

--- src/prisma.module.ts
+++ src/prisma.module.ts
@@ -1,5 +1,7 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { APP_FILTER } from '@nestjs/core';
+import { PrismaExceptionsFilter } from './prisma-exceptions.filter';
 
 @Module({})
 export class PrismaModule {
@@ -7,7 +9,7 @@ export class PrismaModule {
     return {
       global: true,
       module: PrismaModule,
-      providers: [PrismaService],
+      providers: [PrismaService, { provide: APP_FILTER, useClass: PrismaExceptionsFilter }],
       exports: [PrismaService],
     };
   }
