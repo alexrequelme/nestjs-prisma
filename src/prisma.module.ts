f171f79cf630151965bf0c895c0b8716ef2f6c56
AlexRequelme <alexdeveloper09@gmail.com>
2023-10-03 11:24:10 -0500

    Export filter

--- src/prisma.module.ts
+++ src/prisma.module.ts
@@ -1,7 +1,5 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { APP_FILTER } from '@nestjs/core';
-import { PrismaExceptionsFilter } from './prisma-exceptions.filter';
 
 @Module({})
 export class PrismaModule {
@@ -9,7 +7,7 @@ export class PrismaModule {
     return {
       global: true,
       module: PrismaModule,
-      providers: [PrismaService, { provide: APP_FILTER, useClass: PrismaExceptionsFilter }],
+      providers: [PrismaService],
       exports: [PrismaService],
     };
   }
