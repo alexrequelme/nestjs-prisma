171e6045ce55da3d8e731ed1e66737bc34da04b6
AlexRequelme <alexdeveloper09@gmail.com>
2023-10-03 10:28:10 -0500

    Fix pnpm install issue #1

--- src/prisma-exceptions.filter.ts
+++ src/prisma-exceptions.filter.ts
@@ -1,15 +1,17 @@
-import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { ArgumentsHost, Catch, HttpStatus, Type } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { Prisma } from '@prisma/client';
 import { Response } from 'express';
 
-@Catch(Prisma.PrismaClientKnownRequestError)
+type ExceptionData = { statusCode: number; message: string };
+
+@Catch(Prisma.PrismaClientKnownRequestError as Type<any>)
 export class PrismaExceptionsFilter extends BaseExceptionFilter {
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
 
-    const exceptions: Record<string, { statusCode: number; message: string }> = {
+    const exceptions: Record<string, ExceptionData> = {
       P2000: { statusCode: HttpStatus.BAD_REQUEST, message: 'Bad Request' },
       P2002: { statusCode: HttpStatus.CONFLICT, message: 'Resource already exists' },
       P2025: { statusCode: HttpStatus.NOT_FOUND, message: exception.message },
